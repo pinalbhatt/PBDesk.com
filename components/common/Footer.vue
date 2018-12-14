e8577e6930d36b7448b66b99f84ca32eeaa93e4f
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2018-12-14 14:23:32 -0500

    ad

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -2,23 +2,23 @@
   <div>
     <div><ad-responsive></ad-responsive></div>
     <v-footer class="blue darken-2" :clipped-left="true" :fixed="false">
-    <v-layout row wrap align-center>
-      <v-flex xs12>
-        <div class="white--text ml-3 text-md-center">
-          With
-          <v-icon class="red--text">favorite</v-icon>
-          from the desk of <a class="white--text" href="/">Pinal Bhatt</a>.
-        </div>
-      </v-flex>
-    </v-layout>
-    <script>
-      // (adsbygoogle = window.adsbygoogle || []).push({
-      //   google_ad_client: "ca-pub-4686631795727964",
-      //   enable_page_level_ads: true
-      // });
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <div class="white--text ml-3 text-md-center">
+            With
+            <v-icon class="red--text">favorite</v-icon>
+            from the desk of <a class="white--text" href="/">Pinal Bhatt</a>.
+          </div>
+        </v-flex>
+      </v-layout>
   </v-footer>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-4686631795727964",
+        enable_page_level_ads: true
+      });
+    </script>
   </div>
 </template>
 
